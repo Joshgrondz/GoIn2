84ae467b7171dd6f6768093bacac701a62463235
LSC47 <lsc47@pitt.edu>
2025-04-14 21:50:20 -0400

    Started implementing view controllers
    No API calls, just constructor

---
+++ WebApplication1/WebApplication1/Controllers/StudentsInEventsViewController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsInEventsViewController : ControllerBase
+    {
+        private readonly GoIn2Context _context;
+
+        public StudentsInEventsViewController(GoIn2Context context)
+        {
+            _context = context;
+        }
+
+        // IMPLEMENT GET METHODS HERE
+
+    }
+}

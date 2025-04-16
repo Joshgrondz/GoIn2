b061485d320ae01e156ae8ef714cab942ca71238
AidenColberg445 <amc445@pitt.edu>
2025-04-15 21:50:21 -0400

    added one method to get all data

--- WebApplication1/WebApplication1/Controllers/StudentsInEventsViewController.cs
+++ WebApplication1/WebApplication1/Controllers/StudentsInEventsViewController.cs
@@ -1,6 +1,7 @@
 ﻿using WebApplication1.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Controllers
 {
@@ -13,6 +14,28 @@ namespace WebApplication1.Controllers
         public StudentsInEventsViewController(GoIn2Context context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StudentsInEventsView>>> GetStudentProfiles()
+        {
+            return await _context.StudentsInEventsViews.ToListAsync();
+        }
+
+        // Get all students by event ID
+        [HttpGet("{eventid}")]
+        public async Task<ActionResult<List<StudentsInEventsView>>> GetStudentsInEventViewByEventId(int eventid)
+        {
+            var students = await _context.StudentsInEventsViews
+                .Where(loc => loc.EventId == eventid)
+                .ToListAsync();
+
+            if (students == null || students.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return students;
         }
     }
 }

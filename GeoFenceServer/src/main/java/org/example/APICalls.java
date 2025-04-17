f4c27d2e451a32b5024d5ff3328aead0593171a8
Joshua Grondziowski <jlg224@pitt.edu>
2025-04-17 14:42:32 -0400

    Geofence Creation automated. Able to call and make whole geofence from just knowing event ID

--- GeoFenceServer/src/main/java/org/example/APICalls.java
+++ GeoFenceServer/src/main/java/org/example/APICalls.java
@@ -1,4 +1,67 @@
 package org.example;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class APICalls {
+    private static String BASE_URL = "https://webapplication120250408230542-draxa5ckg5gabacc.canadacentral-01.azurewebsites.net";
+
+    //Single item get
+    public static JSONObject makeGetRequestSingleItem(HttpClient client, String endpoint) throws Exception {
+        URI requestUri = URI.create(BASE_URL + endpoint); // Combine base URL and endpoint
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(requestUri)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        /* Printing for testing to see what is retrieved
+        System.out.println("\nGET Request to: " + requestUri);
+        System.out.println("Status Code: " + response.statusCode());
+        System.out.println("Response Body:\n" + response.body());
+         */
+
+        JSONObject jsonObject = null;
+        // Parse JSON (example)
+        if (response.statusCode() == 200) {
+           jsonObject = new JSONObject(response.body());
+        }
+
+        return jsonObject;
+
+    }
+
+    //Multi item Get
+    public static JSONArray makeGetRequestMultiItem(HttpClient client, String endpoint) throws Exception {
+        URI requestUri = URI.create(BASE_URL + endpoint); // Combine base URL and endpoint
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(requestUri)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        /* Printing for testing to see what is retrieved
+        System.out.println("\nGET Request to: " + requestUri);
+        System.out.println("Status Code: " + response.statusCode());
+        System.out.println("Response Body:\n" + response.body());
+         */
+
+        JSONArray jsonArray = null;
+        // Parse JSON (example)
+        if (response.statusCode() == 200) {
+            jsonArray = new JSONArray(response.body());
+        }
+
+        return jsonArray;
+
+    }
 }
--- GeoFenceServer/src/main/java/org/example/Main.java
+++ GeoFenceServer/src/main/java/org/example/Main.java
@@ -1,8 +1,52 @@
 package org.example;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+import java.net.http.HttpClient;
+
+
+/*
+TO-DO-List
+ - Remove Syncronized from needed Classes
+ - 
+*/
+
+
 public class Main {
     public static void main(String[] args) {
-        LocationFetcher lf = new LocationFetcher();
-        lf.run();
+        //Establish Needed variables
+
+        //Client to access web
+        HttpClient client = HttpClient.newHttpClient();
+
+
+        //Print out what event we are doing
+        System.out.println("Event id being tracked: " + args[0] + "\n --------------- \n");
+
+        //Create Geofence
+        EventGeofenceController eventController = EventGeofenceController.CreateGeoFenceController(Integer.valueOf(args[0]), client);
+
+        //get students locations
+
+        //get students who should be part of the class trip
+
+        //Check who has not gotten a location
+
+        //Create users from the students with locations
+
+
+        //Check Locations
+        //Every minute
+        //Only students who are tracked
+        //Notify if outside or broken rule
+
+
+        //Check to see if started location tracking
+
+
+
+
     }
 }
